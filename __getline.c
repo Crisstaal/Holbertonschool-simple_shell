6c6b581e42ec7707abbbef98fcb2fbeb7726b8ff
ajchapman777 <ajchapman777@gmail.com>
2023-12-11 19:23:57 -0800

    files for shell

---
+++ __getline.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	size_t buffsize = 32;
+	char *buffer;
+	char *ptr_copy = NULL;
+	const char *delim = " \n";
+
+	buffer = malloc(sizeof(char) * buffsize);
+
+	while (1)
+	{
+	printf("$ ");
+	getline(&buffer, &buffsize, stdin);
+
+	if (feof(stdin) || ferror(stdin))
+	{
+		printf("Exiting the shell ...\n");
+			break;
+	}
+	
+	printf("%s\n", buffer);
+	}
+
+
+	free(buffer);
+	return (0);
+}
